68093ea0ada33ef6287f34baa748faff5d8295e1
TheCodeNinja <hk4public@gmail.com>
2019-11-25 19:38:30 +0800

    lesson-05 - Touchables

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 import React, { Component } from 'react';
-import { Alert, AppRegistry, View } from 'react-native';
-import Button from 'react-native-button';
+import { 
+    Alert, AppRegistry, View, Image, Text, 
+    TouchableHighlight,
+    TouchableNativeFeedback,
+    TouchableOpacity,
+    TouchableWithoutFeedback 
+} from 'react-native';
 // import App from './App';
 import {name as appName} from './app.json';
 
@@ -17,11 +22,66 @@ export default class App extends Component {
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Button 
-                    style={{ fontSize: 25, color: 'white', backgroundColor: 'green', padding: 15, borderRadius: 20 }}   
-                    onPress={this._onPressButton}>
-                    This is a button
-                </Button>
+                <TouchableHighlight
+                    onPress={this._onPressButton} // Called when the touch is released
+                    underlayColor='red'
+                    onShowUnderlay={() => { // Called immediately after the underlay is shown
+                        alert("onShowUnderlay button !");
+                    }}
+                >
+                    <View style={{ backgroundColor: 'green' }}>
+                        {/* <Image
+                            style={{width: 100, height: 40}}
+                            source={require('./images/touchableHightlightButton.png')}
+                            >
+                        </Image> */}
+                        <Text style={{ color: 'white', padding: 20, fontSize: 18 }}>
+                            TouchableHighlight
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+
+                <TouchableNativeFeedback
+                    onPress={this._onPressButton}
+                    useForeground={false}
+                >
+                    <View style={{ width: 300, height: 50, margin: 20, backgroundColor: 'blue' }}>
+                        <Text style={{ margin: 10, fontSize: 20, color: 'white', textAlign: 'center' }}>
+                            TouchableNativeFeedback
+                        </Text>
+                    </View>
+                </TouchableNativeFeedback>
+
+                <TouchableOpacity
+                    onPress={this._onPressButton}
+                    activeOpacity={0.7}
+                >
+                    <View style={{ width: 200, height: 50, backgroundColor: 'red' }}>
+                        <Text style={{ margin: 10, fontSize: 20, color: 'white', textAlign: 'center' }}>
+                            TouchableOpacity
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableWithoutFeedback
+                    // onPress={this._onPressButton}
+                    onLongPress={() => {
+                        alert("onLongPress");
+                    }}
+                    // onPressIn={() => {
+                    //     alert("onPressIn");
+                    // }}
+                    // onPressOut={() => {
+                    //     alert("onPressOut");
+                    // }}
+                    disabled={false}
+                >
+                    <View style={{ width: 300, height: 50, margin: 20, backgroundColor: 'purple' }}>
+                        <Text style={{ margin: 10, fontSize: 20, color: 'white', textAlign: 'center' }}>
+                            TouchableWithoutFeedback
+                        </Text>
+                    </View>
+                </TouchableWithoutFeedback>
             </View>
         );
     }
